fde32c2060a512bb2b317e86cb4388e4481f27c5
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2024-11-13 15:36:18 +0100

    initial commit

---
+++ examples/galahad.py
@@ -0,0 +1,12 @@
+from clarin_spf import clarin_login
+import requests
+
+
+def main():
+    cookies = clarin_login("https://portal.clarin.ivdnt.org/galahad/home")
+    response = requests.get("https://portal.clarin.ivdnt.org/galahad/api/corpora", cookies=cookies)
+    print(response.json())
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/clarin_spf/__init__.py
@@ -0,0 +1,31 @@
+import time
+from typing import Literal
+
+from playwright.sync_api import sync_playwright
+
+
+def clarin_login(service_url: str, browser_type: Literal["chromium", "firefox", "webkit"] = "webkit") -> dict[str, str]:
+    with sync_playwright() as p:
+        browser = getattr(p, browser_type).launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto(service_url)
+
+        visited_discovery_page = False
+        cookies = {}
+        while True:
+            current_url = page.url
+            # We visited the discovery page and the user will now click on their provider
+            if current_url.startswith("https://discovery.clarin.eu/"):
+                visited_discovery_page = True
+            # If we've already visited the discovery page and returned to the service URL, we're logged in
+            elif visited_discovery_page and current_url == service_url:
+                cookies = {cookie["name"]: cookie["value"] for cookie in context.cookies()}
+                break
+
+            time.sleep(1)
+
+        browser.close()
+
+    return cookies
